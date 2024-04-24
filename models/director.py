6124c46c85e93f8d4cd8c77e90ffa3ae7726a719
Annsrutto <annsrutto567@gmail.com>
2024-04-25 01:48:23 +0300

    updated movie model

--- models/director.py
+++ models/director.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """This module contains the class models for the movie collection API."""
 
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
+
+db = SQLAlchemy()
 
 
 class Director(db.Model):
--- models/movie.py
+++ models/movie.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 """This module contains the class models for the movie collection API."""
 
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, Date, Float, Text, ForeignKey
 
-class Movie(db.Models):
+db = SQLAlchemy()
+
+
+class Movie(db.Model):
     """Contains the class Movie which represents a movie."""
     __tablename__ = 'movies'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
     release_date = db.Column(db.Date)
     rating = db.Column(db.Float)
     duration_minutes = db.Column(db.Integer)
     overview = db.Column(db.Text)
-    genre_id = db.Column(db.Integer, db.ForeignKey('genres.id'))
     director_id = db.Column(db.Integer, db.ForeignKey('directors.id'))
+    genre_id = db.Column(db.Integer, db.ForeignKey('genres.id'))
+    
+
+    def __repr__(self):
+        return f"<Movie(id={self.id}, title='{self.title}')>"
